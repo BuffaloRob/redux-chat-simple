da60866d01a4d7c15062e256a66e3ba278d8ff6a
buffalorob <peckham.rob#gmail.com>
2018-11-28 14:22:04 -0500

    added delete case to reducer

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ const reducer = (state, action) => {
     return {
       messages: state.messages.concat(action.message),
     };
+  } else if (action.type === 'DELETE_MESSAGE') {
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice(action.index + 1, state.messages.length),
+      ],
+    };
   } else {
     return state;
   }
