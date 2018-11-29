c89db8d25e689f1ef74ff69c815718994bd41b33
buffalorob <peckham.rob@gmail.com>
2018-11-29 11:21:46 -0500

    removed testing code

--- src/App.js
+++ src/App.js
@@ -39,29 +39,3 @@ const reducer = (state, action) => {
 
 const initialState = { messages: [] };
 const store = createStore(reducer, initialState);
-
-const listener = () => {
-  console.log('Current state:');
-  console.log(store.getState());
-};
-
-store.subscribe(listener);
-
-const addMessageAction1 = {
-  type: 'ADD_MESSAGE',
-  message: 'How do you read?',
-};
-store.dispatch(addMessageAction1);
-// -> `listener()` is called
-
-const addMessageAction2 = {
-  type: 'ADD_MESSAGE',
-  message: 'I read you loud and clear, Houston.',
-};
-store.dispatch(addMessageAction2)
-
-const deleteMessageAction = {
-  type: 'DELETE_MESSAGE',
-  index: 0,
-};
-store.dispatch(deleteMessageAction);
